83618030aa4ea84c0a0276ab229fb932d441af86
Chikov Sergey <chekistne@mail.ru>
2022-01-20 20:52:15 +0300

    Added csv and xlsx test. Added closable construction.

--- src/test/java/site/kpokogujl/tests/ZipFileTest.java
+++ src/test/java/site/kpokogujl/tests/ZipFileTest.java
@@ -1,6 +1,7 @@
 package site.kpokogujl.tests;
 
 import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Test;
 
@@ -8,36 +9,51 @@ import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class ZipFileTest {
 
-    private ClassLoader cl = ZipFileTest.class.getClassLoader();
+    private final ClassLoader cl = ZipFileTest.class.getClassLoader();
 
     @Test
     void zipTest() throws Exception {
 
-        try (ZipFile zf = new ZipFile(new File(cl.getResource("ZIPArchive.zip").toURI()));) {
+        try (ZipFile zf = new ZipFile(new File(Objects.requireNonNull(cl.getResource("ZIPArchive.zip")).toURI()))) {
             ZipEntry zipEntryPdf = zf.getEntry("PDFFile.pdf");
-            InputStream streamPdf = zf.getInputStream(zipEntryPdf);
-            PDF parsedPdf = new PDF(streamPdf);
+            try (InputStream streamPdf = zf.getInputStream(zipEntryPdf)) {
+                PDF parsedPdf = new PDF(streamPdf);
 
-            assert parsedPdf.text.contains("Пример PDF формы");
-            assert parsedPdf.creator.equals("Writer");
+                assertThat(parsedPdf.text).contains("Пример PDF формы");
+                assertThat(parsedPdf.creator).isEqualTo("Writer");
+            }
 
             ZipEntry zipEntryCsv = zf.getEntry("CSVFile.csv");
-            InputStream streamCsv = zf.getInputStream(zipEntryCsv);
-            CSVReader parsedCsv = new CSVReader(new InputStreamReader(streamCsv));
-            List<String[]> list = parsedCsv.readAll();
-            assert list.contains(new String[] {"First", "1"});
-            assert list.contains(new String[] {"Second", "2"});
+            try (InputStream streamCsv = zf.getInputStream(zipEntryCsv)) {
+                CSVReader parsedCsv = new CSVReader(new InputStreamReader(streamCsv));
+                List<String[]> list = parsedCsv.readAll();
 
-            System.out.println("");
+                assertThat(list)
+                        .hasSize(2)
+                        .contains(
+                                new String[]{"First", "1"},
+                                new String[]{"Second", "2"}
+                        );
+            }
+
+            ZipEntry zipEntryXlsx = zf.getEntry("XLSXFile.xlsx");
+            try (InputStream streamXlsx = zf.getInputStream(zipEntryXlsx)){
+            XLS parsedXlsx = new XLS(streamXlsx);
+
+            assertThat(parsedXlsx.excel.getSheetAt(0).getRow(0).getCell(0).getStringCellValue())
+                    .isEqualTo("a1");
+            assertThat(parsedXlsx.excel.getSheetAt(0).getRow(1).getCell(1).getStringCellValue())
+                    .isEqualTo("b2");
+            }
         }
-
-
-
     }
 }
